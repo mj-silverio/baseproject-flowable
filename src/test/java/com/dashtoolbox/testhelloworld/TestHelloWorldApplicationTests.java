b5e3ea0b3657e410323fa6d946f64496dd0790bb
Catalina-Personal <marksilverio@localhost.localdomain>
2022-06-17 04:12:16 +0800

    Updated unit test

--- src/test/java/com/dashtoolbox/testhelloworld/TestHelloWorldApplicationTests.java
+++ src/test/java/com/dashtoolbox/testhelloworld/TestHelloWorldApplicationTests.java
@@ -2,9 +2,6 @@ package com.dashtoolbox.testhelloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.SpringVersion;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TestHelloWorldApplicationTests {
