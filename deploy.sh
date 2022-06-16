02cc8158de1ff134ebfdac090b530a9551d03619
Catalina-Personal <marksilverio@localhost.localdomain>
2022-06-17 04:03:26 +0800

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 # for windows
-docker login https://artifacts.flowable.com/
+# docker login https://artifacts.flowable.com/
+# docker compose up -d
+
+# mvn package
+# docker build -t bootdocker:1 .
+# docker run -d --name bootdocker -p 8090:8090 bootdocker:1
 
 # for macos/linux
-#sudo sudo service docker start
-#sudo docker login https://artifacts.flowable.com/
-
-docker compose up -d
+sudo service docker start
+sudo docker login https://artifacts.flowable.com/
+sudo docker compose up -d
 
 mvn package
-docker build -t bootdocker:1 .
-docker run -d --name bootdocker -p 8090:8090 bootdocker:1
+sudo docker build -t bootdocker:1 .
+sudo docker run -d --name bootdocker -p 8090:8090 bootdocker:1
