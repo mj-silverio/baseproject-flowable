e0cf797473578de15aa60ba5a2eb9ac0ca100cef
Mark Silverio <markjuliusb.silveriobiz@gmail.com>
2022-06-20 18:21:42 +0800

    Modified deploy, shutdown, and added (extra_hosts: -host.docker.internal:host-gateway) parameter on flowable-engage

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 # for windows
-# docker login https://artifacts.flowable.com/
-# docker compose up -d
+ docker login https://artifacts.flowable.com/
+ docker compose up -d
 
-# mvn package
-# docker build -t bootdocker:1 .
-# docker run -d --name bootdocker -p 8090:8090 bootdocker:1
+ mvn package
+ docker build -t bootdocker:1 .
+ docker run -d --name bootdocker -p 8090:8090 bootdocker:1
 
 # for macos/linux
-sudo service docker start
-sudo docker login https://artifacts.flowable.com/
-sudo docker compose up -d
-
-mvn package
-sudo docker build -t bootdocker:1 .
-sudo docker run -d --name bootdocker -p 8090:8090 bootdocker:1
+#sudo service docker start
+#sudo docker login https://artifacts.flowable.com/
+#sudo docker compose up -d
+#
+#mvn package
+#sudo docker build -t bootdocker:1 .
+#sudo docker run -d --name bootdocker -p 8090:8090 bootdocker:1
--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,11 @@
+# Windows
 docker compose down --remove-orphans -v
 
 docker stop $(docker ps -aqf "name=bootdocker")
 docker rm $(docker ps -aqf "name=bootdocker")
+
+# Macos/Linux
+#sudo docker compose down --remove-orphans -v
+#
+#sudo docker stop $(sudo docker ps -aqf "name=bootdocker")
+#sudo docker rm $(sudo docker ps -aqf "name=bootdocker")
